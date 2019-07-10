1a2059461b27d41a7db9a2310599faf557106a31
Batian <bmuthoga@gmail.com>
2019-07-11 01:36:00 +0300

    add queue from two stacks exercise/solution

---
+++ qfroms/index.js
@@ -0,0 +1,57 @@
+// --- Directions
+// Implement a Queue data structure using two stacks.
+// *Do not* create an array inside of the 'Queue' class.
+// Queue should implement the methods 'add', 'remove', and 'peek'.
+// For a reminder on what each method does, look back
+// at the Queue exercise.
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.add(2);
+//     q.peek();  // returns 1
+//     q.remove(); // returns 1
+//     q.remove(); // returns 2
+
+const Stack = require('./stack');
+
+class Queue {
+  constructor() {
+    this.firstStack = new Stack()
+    this.secondStack = new Stack()
+  }
+
+  add(record) {
+    this.firstStack.push(record)
+  }
+
+  remove() {
+   while (this.firstStack.peek()) {
+     this.secondStack.push(this.firstStack.pop())
+   }
+
+   const result = this.secondStack.pop()
+
+   while (this.secondStack.peek()) {
+     this.firstStack.push(this.secondStack.pop())
+   }
+
+   return result
+    
+  }
+
+  peek() {
+   while (this.firstStack.peek()) {
+     this.secondStack.push(this.firstStack.pop())
+   }
+
+   const result = this.secondStack.peek()
+
+   while (this.secondStack.peek()) {
+     this.firstStack.push(this.secondStack.pop())
+   }
+
+   return result
+  }
+}
+
+module.exports = Queue;
