afa8c807f0112f2e6732d791e00a77a787f1280a
Batian <bmuthoga@gmail.com>
2019-07-20 22:41:14 +0300

    add selectionsort sorting exercise/solution

--- sorting/index.js
+++ sorting/index.js
@@ -16,8 +16,27 @@ function bubbleSort(arr) {
   return arr
 }
 
+/* Using Prove-Me-Wrong Approach */
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfLeast = i
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfLeast]) {
+        indexOfLeast = j
+      }
+    }
+
+    if (i !== indexOfLeast) {
+      let smallerElementFound = arr[indexOfLeast]
+      let currentElement = arr[i]
+
+      arr[i] = smallerElementFound
+      arr[indexOfLeast] = currentElement
+    }
+  }
+
+  return arr
 }
 
 function mergeSort(arr) {
@@ -29,3 +48,20 @@ function merge(left, right) {
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+
+/* Another Solution */
+// function selectionSort(arr) {
+//   for (let i = arr.length - 1; i > -1; i--) {
+//     for (let j = arr.length - 1; j >= arr.length - 1 - i; j--) {
+//       if (arr[j] < arr[j - 1]) {
+//         let smaller = arr[j]
+//         let greater = arr[j - 1]
+
+//         arr[j] = greater
+//         arr[j - 1] = smaller
+//       }
+//     }
+//   }
+
+//   return arr
+// }
